dc16a5e0cec3c981d4e1722eb0c149fa0d6eadb1
anthonymolinari <anthonymolinari106@gmail.com>
2024-12-03 15:31:44 -0800

    updates

--- src/scraper/generate_token.py
+++ src/scraper/generate_token.py
@@ -1,10 +1,23 @@
+import json
+import requests as req
+
+
 # generate auth token for enphase inverter api access
-def generate_token() -> str:
+def generate_token(user, password, envoy_serial):
     print('generating token')
 
+    login_url = 'http://enlighten.enphaseenergy.com/login/login.json?',
+    data = {'user[email]': user, 'user[password]': password}
 
-# test token generator
-if __name__ == "__main__":
-    print('generating test token')
-    token = generate_token()
-    print(f'token: {token}')
+    res = req.post(login_url, data=data)
+    res_data = json.loads(res.text)
+
+    data = {
+        'session_id': res_data['sesson_id'],
+        'serial_num': envoy_serial,
+        'username': user
+    }
+
+    res = req.post('http://entrez.enphaseenergy.com/tokens', json=data)
+    token_raw = res.text
+    return token_raw
